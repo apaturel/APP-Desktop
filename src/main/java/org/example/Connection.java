6ea88152a0f1ef2d60c116300df0e55433968764
arthurpaturel <arthur.paturel@ynov.com>
2023-06-26 14:56:39 +0200

    Test connexion MongoDB

---
+++ src/main/java/org/example/Connection.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Connection {
+    private static Logger logger = LoggerFactory.getLogger(Connection.class);
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://arthurpaturel:arthurpaturel@app-desktop.cwfrlkx.mongodb.net/?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            List<Document> databases = mongoClient.listDatabases().into(new ArrayList<>());
+            databases.forEach(db -> logger.info("Test", db.toJson()));
+        }
+    }
+}
